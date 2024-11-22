399e89039cc738521fc40cef68927aa4accb2cef
Markp33 <marrrk700@yahoo.com>
2024-11-22 11:46:11 +0100

    index+header

--- header.php
+++ header.php
@@ -1,34 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-
+ 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./src/output.css" rel="stylesheet">
+    <link href="https://cdn.tailwindcss.com" rel="stylesheet">
     <title>Focus6</title>
 </head>
-
-
+ 
 <body class="flex flex-col min-h-screen">
+    
     <header class="bg-gray-400 text-white rounded-full mx-8 fixed top-4 left-0 right-0 z-50">
         <div class="container mx-auto flex items-center justify-between p-4">
-
             <a href="index.php" class="flex-shrink-0">
                 <img class="h-10" src="" alt="Logo">
             </a>
-
-            <nav>
-                <ul class="flex space-x-4">
-                    <li><a href="index.php" class="hover:text-black">Home</a></li>
-                    <li><a href="spiegelspel.php"
-                            class="hover:text-black border-l border-white pl-4">Spiegelspelconcept</a></li>
-                    <li><a href="dienst.php" class="hover:text-black border-l border-white pl-4">Dienstregeling</a></li>
-                    <li><a href="contact.php" class="hover:text-black border-l border-white pl-4">Contact</a></li>
+ 
+            <button id="menuButton" class="block md:hidden focus:outline-none">
+                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+            </button>
+ 
+            
+            <nav id="menu" class="hidden md:flex md:items-center space-x-4">
+                <ul class="flex flex-col md:flex-row md:space-x-4">
+                    <li><a href="index.php"         class="hover:text-black">Home</a></li>
+                    <li><a href="spiegelspel.php"   class="hover:text-black border-l border-white pl-4">Spiegelspelconcept</a></li>
+                    <li><a href="dienst.php"        class="hover:text-black border-l border-white pl-4">Dienstregeling</a></li>
+                    <li><a href="contact.php"       class="hover:text-black border-l border-white pl-4">Contact</a></li>
                 </ul>
             </nav>
-            <a href="tel:06456789" class="text-xl font-medium hover:text-black mr-8">
+ 
+            
+            <a href="tel:06456789" class="text-xl font-medium hover:text-black hidden md:block">
                 0612345678
             </a>
-
-
+        </div>
     </header>
+ 
+ 
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            const currentPath = window.location.pathname.split("/").pop();
+            document.querySelectorAll("nav a").forEach((link) => {
+                if (link.getAttribute("href") === currentPath) {
+                    link.classList.add("text-black");
+                }
+            });
+        });
+    </script>
+ 
+ 
+    <script>
+    const menuButton = document.getElementById('menuButton');
+    const menu = document.getElementById('menu');
+ 
+    menuButton.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
+    </script>
+
+
+</body>
+ 
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once 'header.php'; ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,51 +13,120 @@
 
 <body>
 <div id="default-carousel" class="relative w-full" data-carousel="slide">
+  
 
-    <div class="relative h-[20rem] overflow-hidden md:h-[600px]">
-        <div class="duration-700 ease-in-out" data-carousel-item>
-            <img src="place.png" class="absolute block w-full z-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="eerste image">
-            <div className="w-full absolute top-0 left-0 text-center mt-10 z-0">
+
+<div class="relative h-[18rem] overflow-hidden md:h-[600px]">
+            <button id="menuButton" class="block md:hidden focus:outline-none">
+                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                </svg>
+            </button>
+    <div class="duration-700 ease-in-out" data-carousel-item>
+        <img src="place.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="First Image">
+        <div class="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50">
+            <h2 class="text-4xl font-bold text-white uppercase">Focus6</h2>
+        </div>
     </div>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="place1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="tweedeimage">
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="place2.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="derde image">
+    
+    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+        <img src="place1.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Second Image">
+        <div class="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50">
+            <h2 class="text-4xl font-bold text-white uppercase">Focus6</h2>
         </div>
     </div>
+    
+    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+        <img src="place2.png" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Third Image">
+        <div class="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50">
+            <h2 class="text-4xl font-bold text-white uppercase">Focus6</h2>
+        </div>
+    </div>
+</div>
 
     
+
+
+
+
     <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-label="Slide 3" data-carousel-slide-to="2"></button>
     </div>
-
     
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer" data-carousel-prev>
+        <span class="inline-flex items-center justify-center rounded-full w-10 h-10 bg-white/30 dark:bg-gray-800/30">
+            <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-width="2" d="M5 1 1 5l4 4" />
             </svg>
-            <span class="sr-only">Previous</span>
         </span>
     </button>
-
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer" data-carousel-next>
+        <span class="inline-flex items-center justify-center rounded-full w-10 h-10 bg-white/30 dark:bg-gray-800/30">
+            <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-width="2" d="M1 9l4-4-4-4" />
             </svg>
-            <span class="sr-only">Next</span>
         </span>
     </button>
 </div>
 
+
+
+
+<div class="flex flex-col items-center space-y-[5rem] my-[15rem]">
+
+        <div class="flex space-x-[20rem]">
+            <div class="h-[25rem] w-[25rem]">
+                <h2 class="text-xl font-bold mt-8">Focus6</h2>
+                <p class="mt-[5rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem excepturi asperiores ratione nemo quae animi, pariatur quas soluta, praesentium necessitatibus deserunt ducimus maiores autem quaerat magnam dolorum molestiae? Reiciendis, quam.</p>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-10">
+                        Click Me
+                    </button>
+            </div>
+            <div class="h-[25rem] w-[25rem] bg-black"></div>
+        </div>
+
+        <div class="flex space-x-[20rem]">
+            <div class="h-[25rem] w-[25rem] bg-black"></div>
+            <div class="h-[25rem] w-[25rem]">
+                <h2 class="text-xl font-bold mt-8">Spiegelspel</h2>
+                <p class="mt-[5rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem excepturi asperiores ratione nemo quae animi, pariatur quas soluta, praesentium necessitatibus deserunt ducimus maiores autem quaerat magnam dolorum molestiae? Reiciendis, quam.</p>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-10">
+                        Click Me
+                    </button>
+            </div>
+            
+        </div>
+
+        <div class="flex space-x-[20rem]">
+            <div class="h-[25rem] w-[25rem]">
+                <h2 class="text-xl font-bold mt-8">Dienstverlening</h2>
+                <p class="mt-[5rem]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem excepturi asperiores ratione nemo quae animi, pariatur quas soluta, praesentium necessitatibus deserunt ducimus maiores autem quaerat magnam dolorum molestiae? Reiciendis, quam.</p>
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-10">
+                        Click Me
+                    </button>
+            </div>
+            <div class="h-[25rem] w-[25rem] bg-black"></div>
+        </div>
+
+    </div>
 </body>
 
+
+
+
+
 <script>
+
+    const menuButton = document.getElementById('menuButton');
+        const menu = document.getElementById('menu');
+    
+        menuButton.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+
     const carouselElement = document.getElementById('default-carousel');
 
     const items = [
